136524be3db5a6a3de752d598ec950b42d4f93ba
Tatyana <mta@inrecolan.com>
2025-07-31 14:56:34 +0300

    Completed homework #4 (task-1, task-2, task-3)

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -3,3 +3,12 @@
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
+  const salary = 1000;
+  let grade;
+  
+  if(salary >= 1000) {
+      grade = 'middle'
+  } else grade = 'junior'
+  
+  console.log(grade) // middle
+  
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,20 @@
   преобразовываясь в number
 
 */
+const minAge = 18;
+  const maxAge = 60;
+  let age = "59"; // 17, 18, 19, 60, 61, "61", " 61", "61 ", " 61 ", "6 1", "a61b", "asd", "", " "
+
+
+  if(isNaN(age) || typeof age != 'number' && typeof age != 'string') {
+    console.log('Incorrect data type')
+  } else if( typeof age == 'string' && age.trim() == '') {
+    console.log('Incorrect data type')
+  } else
+  if(age < minAge) {
+    console.log("You don't have access cause your age is " + age + ". It's less then " + minAge)
+  } else if (age >= minAge && age < maxAge) {
+    console.log("Welcome !")
+  } else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel")
+  } else console.log("Technical work")
